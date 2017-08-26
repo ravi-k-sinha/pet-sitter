18b5f0c59b9bfe56da48275ad149dceb49b25f84
Ravi K Sinha <ravi.k.sinha.dev@gmail.com>
2017-08-26 14:08:40 +0530

    Completed 'TODO 5,6,7' in MtBeanTest in 02-ps-container-01-practice_test

--- 02-ps-container-01-practice/src/test/java/com/ps/beans/others/MtBeanTest.java
+++ 02-ps-container-01-practice/src/test/java/com/ps/beans/others/MtBeanTest.java
@@ -6,7 +6,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.io.Closeable;
+import java.io.IOException;
+
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by iuliana.cosmina on 3/26/16.
@@ -16,13 +20,35 @@ public class MtBeanTest {
     private Logger logger = LoggerFactory.getLogger(MtBeanTest.class);
 
     @Test
-    public void testConfig() {
+    public void testConfig() throws IOException {
         //TODO 6. Modify this class to use the new set of configuration files, created by resolving TODO 5.
         //TODO 7. Try to use wildcards as well.
         ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:spring/others/sample-config-01.xml");
 
         MultipleTypesBean mtBean = (MultipleTypesBean) ctx.getBean("mtBean");
         assertNotNull(mtBean);
+
+        //expected no of beans in the context
+        int beanCount1 = ctx.getBeanDefinitionNames().length;
+        ((Closeable)ctx).close();
+
+        ctx = new ClassPathXmlApplicationContext(
+                "classpath:spring/others/sample-config-01-cfg-01-mtype.xml",
+                "classpath:spring/others/sample-config-01-cfg-02-converter.xml",
+                "classpath:spring/others/sample-config-01-cfg-03-beans.xml",
+                "classpath:spring/others/sample-config-01-cfg-04-factory.xml"
+        );
+        int beanCount2 = ctx.getBeanDefinitionCount();
+        ((Closeable)ctx).close();
+
+        ctx = new ClassPathXmlApplicationContext(
+                "classpath:spring/others/*-cfg-*.xml"
+        );
+        int beanCount3 = ctx.getBeanDefinitionCount();
+
+        assertTrue("Counts through various methods must be same", beanCount1 == beanCount2);
+
+        assertTrue("Counts through various methods must be same", beanCount1 == beanCount3);
 
     }
 }
