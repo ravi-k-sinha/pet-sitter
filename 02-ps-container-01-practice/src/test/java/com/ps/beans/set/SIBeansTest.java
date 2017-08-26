27ca9fc7fb5e21290e129efadca7addb47ca42f8
Ravi K Sinha <ravi.k.sinha.dev@gmail.com>
2017-08-26 14:19:12 +0530

    Completed 'TODO 4' in SIBeansTest in 02-ps-container-01-practice_test

--- 02-ps-container-01-practice/src/test/java/com/ps/beans/set/SIBeansTest.java
+++ 02-ps-container-01-practice/src/test/java/com/ps/beans/set/SIBeansTest.java
@@ -6,6 +6,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.Map;
+
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
@@ -26,5 +29,17 @@ public class SIBeansTest {
         }
 
         //TODO 4. Retrieve beans of types ComplexBean and make sure their dependencies were correctly set.
+        ComplexBeanImpl complexBean0 = ctx.getBean("complexBean0", ComplexBeanImpl.class);
+        assertNotNull(complexBean0.getSimpleBean());
+        assertFalse(complexBean0.isComplex());
+
+        ComplexBeanImpl complexBean1 = ctx.getBean("complexBean1", ComplexBeanImpl.class);
+        assertNotNull(complexBean1.getSimpleBean());
+        assertTrue(complexBean1.isComplex());
+
+        ComplexBean2Impl complexBean2 = ctx.getBean("complexBean2", ComplexBean2Impl.class);
+        assertNotNull(complexBean2.getSimpleBean());
+        assertTrue(complexBean2.isComplex());
+
     }
 }
